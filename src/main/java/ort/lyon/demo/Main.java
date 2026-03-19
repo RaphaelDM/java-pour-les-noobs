b15ec9c57afd10fce5332ed290859828c955d519
Raphael DANJARD <r.danjardmaza@lyon.ort.asso.fr>
2026-03-19 09:55:59 +0100

    feat : ajout swager pour visuel api et test endpoint

--- src/main/java/ort/lyon/demo/Main.java
+++ src/main/java/ort/lyon/demo/Main.java
@@ -8,5 +8,7 @@ public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
         System.out.println("Application démarrée !");
+        System.out.println("Pour accéder à la documentation Swagger, rendez-vous sur : http://localhost:8080/swagger-ui.html");
+        System.out.println("Pour accéder à l'API, rendez-vous sur : http://localhost:8080/books");
     }
 }
